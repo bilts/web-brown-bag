131ba512522d1c0d3d363cba0e35a505c267afe3
bilts <patrick@patrickquinn.net>
2011-03-11 17:30:13 -0500

    Initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebBrownbag::Application.config.secret_token = '1013dccd83145f877cc6c5df36764b7a5771245514dc83ecc2738f0dd701118cd67caa6be351b4a9e4652aa7fa57dfd73e6677ad390baa3b9552a47f64884991'
