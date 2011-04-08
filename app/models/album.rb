d5a0e393784e66b67b5253607a3f9317d24ef109
bilts <patrick@patrickquinn.net>
2011-04-08 18:12:33 -0400

    Implement album, artist, and song models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,7 @@
 class Album < ActiveRecord::Base
   has_many :songs
   belongs_to :artist
+  default_scope order('name')
 
   def to_s
     name
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :album
+  default_scope order('name')
 
   def to_s
     name
+  end
+
+  def <=>(other)
+    name.gsub(/^(the|a|an) /i, '') <=> other.name.gsub(/^(the|a|an) /i, '')
   end
 end
