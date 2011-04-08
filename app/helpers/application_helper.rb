26df0de3b0b6b4eeb6602c57a05c7d5ccfb826c1
bilts <patrick@patrickquinn.net>
2011-04-08 18:13:11 -0400

    Add helper methods for creating HTML lists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def html_list(type, elements, options = {})
+    if elements.blank?
+      ""
+    else
+      lis = elements.map { |x| content_tag("li", x) }
+      content_tag(type, lis, options, false)
+    end
+  end
+
+  def ul(*args)
+    html_list("ul", *args)
+  end
+
+  def ol(*args)
+    html_list("ol", *args)
+  end
 end
